539203f2679607620019a2fcc3230821f3ddf93a
luke-h1 <luke.howsam@yahoo.com>
2020-06-24 15:00:27 +0100

    added app.js

---
+++ Module5-CSS-Grid/jake-elwood-project/app.js
@@ -0,0 +1,8 @@
+const closeButton = document.querySelector('.close-nav');    
+const nav = document.querySelector('.nav'); 
+
+
+closeButton.addEventListener("click", ()=> { 
+    nav.classList.remove()
+}); 
+
