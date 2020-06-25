2f96e92af943d62683b82614f4e8c77959597694
luke-h1 <luke.howsam@yahoo.com>
2020-06-25 12:57:10 +0100

    update:

--- Module5-CSS-Grid/jake-elwood-project/app.js
+++ Module5-CSS-Grid/jake-elwood-project/app.js
@@ -1,18 +1,16 @@
+//close Navigation JS
+const closeButton = document.querySelector(".close-nav");
+const nav = document.querySelector(".nav");
 
-//close Navigation JS 
-const closeButton = document.querySelector('.close-nav');    
-const nav = document.querySelector('.nav'); 
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("navigation-open");
+});
 
+// Open hamburger menu JS
 
-closeButton.addEventListener("click", ()=> { 
-    nav.classList.remove('navigation-open') 
-}); 
-
-// Open hamburger menu JS  
-
-const openButton = document.querySelector('.open-nav') ; 
-openButton.addEventListener("click", () => { 
-    nav.classList.add('navigation-open'); 
-}); 
+const openButton = document.querySelector(".open-nav");
+openButton.addEventListener("click", () => {
+  nav.classList.add("navigation-open");
+});
 
 // TO-DO: FIX WIDTH ON HAMBURGER MENU (I.E. SO IT DOESN'T FILL UP FULL SCREEN )
